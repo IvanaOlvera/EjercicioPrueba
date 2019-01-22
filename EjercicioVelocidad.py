e509a328dbf9e34376f38f9b9e12254b6e71aacb
IvanaOlvera <46821616+IvanaOlvera@users.noreply.github.com>
2019-01-22 11:04:38 -0600

    Ivana Olvera Mérida, Grupo 03

---
+++ EjercicioVelocidad.py
@@ -0,0 +1,8 @@
+#Autor: Ivana Olvera Mérida
+#Calcula velocidad promedio, dado el tiempo y la distancia
+
+distancia= int(input("Teclea la distancia de tu viaje en kms enteros: "))
+tiempo= int(input("Teclea el tiempo de tu viaje en horas enteras: "))
+
+velocidad= distancia / tiempo
+print("La velocidad promedio:", velocidad, "km/h")
